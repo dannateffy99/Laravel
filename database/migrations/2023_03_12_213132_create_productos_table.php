13099d2446c4cc7c2b38d3b106b71bae881755c1
dannateffy99 <127898924+dannateffy99@users.noreply.github.com>
2023-03-15 14:48:41 -0500

    Add files via upload

---
+++ database/migrations/2023_03_12_213132_create_productos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_categoria');
+            $table->foreign('id_categoria')->references('id')->on('categorias');
+            $table->string('name');
+            $table->string('descripcion');
+            $table->integer('stock');
+            $table->double('precio',8,2);
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
